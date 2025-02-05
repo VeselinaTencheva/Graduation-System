227470059fbc3f0764a25c779bae8e33e0e63131
Veselina Tencheva <veselinatencheva9@gmail.com>
2025-02-05 14:00:34 +0200

    Initial commit: Project setup

---
+++ src/test/java/com/nbu/Graduation_System/GraduationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbu.Graduation_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
